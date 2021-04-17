c35df0df274444be409b616292e4774c26a23878
hwj <huangweijun1001@gmail.com>
2021-04-17 15:43:57 +0800

    complete webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -26,8 +26,8 @@ void get_URL(const string &host, const string &path) {
         cout<<sock1.read();
     sock1.close();
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    //cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    //cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
