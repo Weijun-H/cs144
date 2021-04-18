aac80516fd2cf10bc55f13a4273ae16c5ccf187a
hwj <huangweijun1001@gmail.com>
2021-04-18 22:12:07 +0800

    update some private members in StreamReassembler Class

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,6 +5,7 @@
 
 #include <cstdint>
 #include <string>
+#include <set>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -24,10 +25,12 @@ class typeUnassembled{
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-
-    std::deque<char> q{};
+    std::set<typeUnassembled> _Unassembled;
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    size_t _firstUnassembled;
+    size_t _nUnassembled;
+    bool _eof;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
